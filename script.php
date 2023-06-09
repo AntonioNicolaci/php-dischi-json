f85e10c9427ffb8ce397d7ed14dc36438cac2f91
antonionicolaci <antonionicolaci48@gmail.com>
2023-06-09 14:50:42 +0200

    add file

---
+++ script.php
@@ -0,0 +1,40 @@
+<?php
+$musicDisc = [
+    [
+        'nameDisc' => 'A quiet place',
+        'artist' => 'Marco Beltrani',
+        'year' => '2018',
+        'imgPath' => '.img/aquietplace.jpeg', 
+    ],
+    [
+        'nameDisc' => 'A cure for wellness',
+        'artist' => 'Benjamin Wallfisch',
+        'year' => '2017',
+        'imgPath' => '.img/cure.jpeg', 
+    ],
+    [
+        'nameDisc' => 'Fly, una mattina',
+        'artist' => 'Ludovico Eindaudi',
+        'year' => '2011',
+        'imgPath' => '.img/einaudi.jpeg', 
+    ],
+    [
+        'nameDisc' => 'Split',
+        'artist' => 'West Thordson',
+        'year' => '2016',
+        'imgPath' => '.img/split.jpeg', 
+    ],
+    [
+        'nameDisc' => 'Stranger Things',
+        'artist' => 'Kyle Dixoh & Michael Stein',
+        'year' => '2016',
+        'imgPath' => '.img/stranger.jpeg', 
+    ],
+    [
+        'nameDisc' => 'Young & Beautiful',
+        'artist' => 'Lana del Rey',
+        'year' => '2013',
+        'imgPath' => '.img/young.jpeg', 
+    ],
+]
+?>
